b45c18c0ab87f45d2dc13ed024c87ab64821c816
Lukas Samby <lukas.samby@gmail.com>
2023-11-06 10:31:51 +0100

    Added Express Gateway. Added EG to docker-compose.

--- live-match/src/index.js
+++ live-match/src/index.js
@@ -7,19 +7,19 @@ db.connect(app);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/live-match/', (req, res) => {
     res.send('Hello worlds!');
 })
 
 // add routes
 const createRoute = require('./routes/create');
-app.use('/create', createRoute);
+app.use('/live-match/create', createRoute);
 const searchRoute = require('./routes/search');
-app.use('/', searchRoute);
+app.use('/live-match/', searchRoute);
 const getRoute = require('./routes/get');
-app.use('/', getRoute);
+app.use('/live-match/', getRoute);
 const joinRoute = require('./routes/join');
-app.use('/', joinRoute);
+app.use('/live-match/', joinRoute);
 
 app.listen(port, () => console.log(`App is listening on ${port}`));
 
